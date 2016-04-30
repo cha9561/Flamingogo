9384b6c350e351f095afb7d9cb6b36bd56100156
minji_son <minji_son@minji_son-PC>
2016-04-30 12:56:41 +0900

    예약 추가

--- src/main/java/com/sist/flamingogo/TourController.java
+++ src/main/java/com/sist/flamingogo/TourController.java
@@ -9,7 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
 import com.tour.dao.*;
-
+import java.util.*;
+import java.text.*;
 
 @Controller("tc")
 public class TourController {
@@ -67,17 +68,64 @@ public class TourController {
 	@RequestMapping("tour_date.do")
 	public String tour_date(HttpServletRequest req)
 	{
-		
-		
-		req.setAttribute("jsp", "../tour/reserve.jsp");
-		return "user/main/main.jsp";
+		String strYear=req.getParameter("year");
+    	String strMonth=req.getParameter("month");
+    	Date date=new Date();
+    	SimpleDateFormat sdf=new SimpleDateFormat("yyyy-M-d");//09 
+    	StringTokenizer st=new StringTokenizer(sdf.format(date), "-");
+    	String sy=st.nextToken();
+    	String sm=st.nextToken();
+    	String sd=st.nextToken();
+    	
+    	if(strYear==null)
+    		strYear=sy;
+    	if(strMonth==null)
+    		strMonth=sm;
+    	
+    	int year=Integer.parseInt(strYear);
+    	int month=Integer.parseInt(strMonth);
+    	int day=Integer.parseInt(sd);
+    	
+    	String[] strWeek={"일","월","화","수","목","금","토"};
+    	int[] lastDay={
+    		31,28,31,30,31,30,
+    		31,31,30,31,30,31
+    	};
+    	
+    	int total=((year-1)*365)+((year-1)/4)-((year-1)/100)
+    			 +((year-1)/400);
+    	if(((year%4==0)&&(year%100!=0))||(year%400==0))
+    	{
+    		lastDay[1]=29;
+    	}
+    	else
+    	{
+    		lastDay[1]=28;
+    	}
+    	
+    	for(int i=0;i<month-1;i++)
+    	{
+    		total+=lastDay[i];
+    	}
+    	
+    	total++;
+    	int week=total%7;
+    	req.setAttribute("year", year);
+    	req.setAttribute("month", month);
+    	req.setAttribute("day", day);
+    	req.setAttribute("week", week);
+    	req.setAttribute("lastDay", lastDay[month-1]);
+    	List<String> weekList=new ArrayList<String>();
+    	for(int i=0;i<7;i++)
+    	{
+    		weekList.add(strWeek[i]);
+    	}
+    	req.setAttribute("weekList", weekList);
+		return "user/tour/tour_date.jsp";
 	}
-	@RequestMapping("tour_info.do")
+	@RequestMapping("tour_inwon.do")
 	public String tour_info(HttpServletRequest req)
 	{
-		
-		
-		req.setAttribute("jsp", "../tour/reserve.jsp");
-		return "user/main/main.jsp";
+		return "user/tour/tour_inwon.jsp";
 	}
 }
